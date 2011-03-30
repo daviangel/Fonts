7847456d4d3e91c8dd92b725a9a91e4e1a21c0da
Jaime Moreno <daviangel@hotmail.com>
2011-03-30 05:32:20 -0700

    first commit

---
+++ Fonts-main.m
@@ -0,0 +1,16 @@
+//
+//  Fonts-main.m
+//  Fonts
+//
+//  Created by Jaime Moreno on 12/20/10.
+//  Copyright 2010 Jologames. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
